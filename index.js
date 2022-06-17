8412cc5388dfdf4410dcd3493f20e46c24416c01
danMihel <d.mihel78@gmail.com>
2022-06-17 19:07:29 +0300

    master

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener('DOMContentLoaded', function()   {
+    console.log('start');
     fildOne = document.getElementById('strOne');
-    str1 = ["Visualize your data"];
-    str2 = ["Rationalize your portfolio of assets",];
-    str3 = ['You know what your business does.'];
-    str4 = ['You have a list of technologies used.'];
-    str5 = ['You need to answer questions like: '];
+    str1 = ["Visualize "];
+    str2 = ["your data"];
+    str3 = ["Rationalize "];
+    str4 = ["your portfolio of assets",];
+    str5 = ['You know what your business does.\n',
+            'You have a list of technologies used.\n',
+            'You need to answer questions like:\n'
+            ];
     str6 = ['How to reduce technology debt?\n',
             'Where is the record of truth for Customer profile?\n',
             'How to mitigate risk of license/version expiration?\n',
@@ -30,31 +33,32 @@ document.addEventListener('DOMContentLoaded', () => {
     function playAnimation(){
         heroAnimation("strOne",str1, 100);
         setTimeout(function(){
-            document.getElementById("strOne").innerHTML = "";
-            heroAnimation("strOne",str2, 100);
+            heroAnimation("strTwo",str2, 100);
             setTimeout(function(){
-                heroAnimation("strTwo", str3, 100);
+                document.getElementById("strOne").innerHTML = "";
+                heroAnimation("strOne",str3, 150);
                 setTimeout(function(){
-                    heroAnimation("strThree",str4, 100);
-                    setTimeout(function(){
-                        heroAnimation("strFore",str5, 100);
-                        setTimeout(function(){
-                            heroAnimation("strFive",str6, 50);  
-                            setTimeout(function(){
-                                document.getElementById("strOne").innerHTML = "";
-                                document.getElementById("strTwo").innerHTML = "";
-                                document.getElementById("strThree").innerHTML = "";
-                                document.getElementById("strFore").innerHTML = "";
-                                document.getElementById("strFive").innerHTML = "";
-                            },30000);                                            
-                        },4000);               
+                    document.getElementById("strTwo").innerHTML = "";
+                    heroAnimation("strOne",str4, 150);
+                    setTimeout(function(){                        
+                        heroAnimation("strThree",str5, 100);
+                        setTimeout(function(){                        
+                            heroAnimation("strFore",str6, 100);
+                        },12000);
                     },4000);
-                },4000);
-            },6000);           
-        },4000);              
+                },3000);
+            },2000);
+        },1000);
         }
     playAnimation();
-    setTimeout(function(){
+    setInterval(function(){
+        document.getElementById("strOne").innerHTML = "";
+        document.getElementById("strThree").innerHTML = "";
+        document.getElementById("strFore").innerHTML = "";
         playAnimation();
-    },60000);
+    },90000); 
+
+    
 });
+
+
